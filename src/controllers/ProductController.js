129b57b66d382238d6e907191ed72fd21926ad2c
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-22 18:09:26 +0700

    neu commit

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,20 +4,32 @@ const {MongooseToObject}=require('../ultil/mongoose')
 const listCaterology=require('../ultil/caterology')
 
 class ProductController{
-    async index(req,res){
+    async index(req,res){  // get all product
         try{
             const allProduct=await Product.find({})
-            res.render('products',{products:MultipleMongooseToObject(allProduct),caterology:'All Product'})
+            allProduct.forEach(product=>{
+                product.quantity=product.info.reduce((total,item)=>{
+                    return total+item.quantity
+                },0)
+                product.discount=parseInt(100-(product.pricePromotion/product.price)*100)
+            })
+            
+            res.render('products',{caterology:listCaterology,caterologyName:'All Product',products:MultipleMongooseToObject(allProduct)})
+        
         }
         catch(e){
             res.render('404')
         }
     }
-    async detail(req,res){
+    async detail(req,res){   // get detail product 
         const slug=req.params
         console.log(slug)
         try{
-            const allProduct=await Product.findOne({slug:slug.slug})
+            var allProduct=await Product.findOne({slug:slug.slug})
+            allProduct.quantity=allProduct.info.reduce((total,item)=>{
+                return total + item.quantity
+            },0)
+            allProduct.discount=parseInt(100-(allProduct.pricePromotion/allProduct.price)*100)
             if(allProduct){
                 res.render('product-detail',{product:MongooseToObject(allProduct)})
             }
@@ -40,7 +52,7 @@ class ProductController{
             try{
                 const allProduct=await Product.find({type:slug.caterology})
                 console.log(allProduct)
-                res.render('products',{products:MultipleMongooseToObject(allProduct),caterology:slug.caterology.toUpperCase()})
+                res.render('products',{products:MultipleMongooseToObject(allProduct),caterology:slug.caterology.toUpperCase(),caterology:listCaterology})
             }
             catch(e){
                 res.render('404')
