3031853ce619a939330adbd75ff1d01ead275f4f
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-27 22:52:37 +0700

    sdfsd

--- src/routes/login.js
+++ src/routes/login.js
@@ -4,4 +4,5 @@ const route=express.Router()
 const loginController=require('../controllers/LoginController')
 
 route.get('/',loginController.index)
+route.post('/',loginController.validation)
 module.exports=route
