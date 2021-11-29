32d57c3de885c782c7fa2da0b616d7e301e5acb5
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2021-11-29 11:15:17 +0700

    sdfdf

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,17 @@
-const productRouter=require('./product')
-const siteRouter=require('./site')
-const checkoutRouter=require('./checkout')
+const ProductRouter=require('./product')
+const SiteRouter=require('./site')
+const CheckoutRouter=require('./checkout')
 const ContactRouter=require('./contact')
 const BlogRouter=require('./blog')
-const cartRouter=require('./cart')
-const loginRouter=require('./login')
+const CartRouter=require('./cart')
+const LoginRouter=require('./login')
 function route(app){
-    app.use('/cart',cartRouter)
-    app.use('/product',productRouter)
-    app.use('/check-out',checkoutRouter)
+    app.use('/cart',CartRouter)
+    app.use('/product',ProductRouter)
+    app.use('/check-out',CheckoutRouter)
     app.use('/contact',ContactRouter)
     app.use('/blog',BlogRouter)
-    app.use('/login',loginRouter)
-    app.use('/',siteRouter)
-
+    app.use('/login',LoginRouter)
+    app.use('/',SiteRouter)
 }
 module.exports=route
