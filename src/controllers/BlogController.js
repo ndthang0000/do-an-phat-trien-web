a48374b6b6d6972d11d51ff1ac3d0f134d11f2b4
ndthang0000 <85822212+ndthang0000@users.noreply.github.com>
2022-01-12 10:34:51 +0700

    fix cart

--- src/controllers/BlogController.js
+++ src/controllers/BlogController.js
@@ -1,5 +1,4 @@
 
-
 const index=async(req,res)=>{
     res.render('blog')
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,7 +8,7 @@ const AuthRouter=require('./auth')
 const MeRouter=require('./me')
 const Register=require('./register')
 
-const {meMiddleWare,prventLoginMiddleWare}=require('../middlewares/customMiddleWare')
+const {meMiddleWare,preventLoginMiddleWare}=require('../middlewares/customMiddleWare')
 
 function route(app){
     app.use('/me',meMiddleWare,MeRouter)
@@ -17,8 +17,8 @@ function route(app){
     app.use('/check-out',CheckoutRouter)
     app.use('/contact',ContactRouter)
     app.use('/blog',BlogRouter)
-    app.use('/login',prventLoginMiddleWare,AuthRouter)
-    app.use('/register',prventLoginMiddleWare,Register)
+    app.use('/login',preventLoginMiddleWare,AuthRouter)
+    app.use('/register',preventLoginMiddleWare,Register)
     app.get('/logout', function(req, res){
         req.logout();
         res.redirect('/login');
